91580fb72ee21fd2bd055996dedbb1aaa38df368
João Nelson <joaonelson08@gmail.com>
2024-03-24 16:36:51 -0300

    terceiro commit do app

--- src/App.jsx
+++ src/App.jsx
@@ -7,21 +7,21 @@ export default function app() {
   return (
     <main>
       <section>
-        <div>
+        <div className="bg-orange">
           <img src={iconSedans} alt="ícone dos carros sedan" />
           <h2 className="text-3xl">Sedans</h2>
           <p>Escolha um sedan pelo seu preço acessível e excelente economia de combustível. Ideal para dirigir na cidade ou na sua próxima viagem.</p>
           <a href="https://www.webmotors.com.br/sedans" target="_blank">Ver mais</a>
         </div>
 
-        <div>
+        <div className="bg-green">
         <img src={iconSuvs} alt="Ícone dos carros SUV" />
           <h2>SUVs</h2>
           <p>Escolha um SUV pelo seu interior espaçoso, poder e versatilidade. Perfeito para passar as férias em família ou para aventuras off-road.</p>
           <a href="https://www.webmotors.com.br/carros/estoque?idcmp=s08%3Ac03%3A8bcd6c70eb2a4017aac2e34222708902&tipoveiculo=carros&anunciante=Loja%7CConcessionária&carroceria=Utilitário+esportivo&gclsrc=aw.ds&gad_source=1&gclid=CjwKCAjwnv-vBhBdEiwABCYQA7IZp-BWtNKF9QOin6TfRDJkwrhA8eche_LIFeoIlJpsD9nzV4ydLhoCmLIQAvD_BwE" target="_blank">Ver mais</a>
         </div>
 
-        <div>
+        <div className="bg-dark-green">
           <img src={iconLuxury} alt="ícone dos carros de luxo" />
           <h2>Luxuosos</h2>
           <p>Passeie nos melhores carros sem os preços exorbitantes. Desfrute do conforto aprimorado de um aluguel de luxo e chegue com estilo.</p>
--- tailwind.config.js
+++ tailwind.config.js
@@ -8,11 +8,15 @@ export default {
     colors: {
       'orange': '#e28625',
       'green': '#006971',
-      'dark': '#004140',
+      'dark-green': '#004140',
       'offwhite': 'rgba(255, 255, 255, 0.75)',
       'white': '#fff',
       'offwhitebg': '#f2f2f2'
 
+    },
+    fontFamily: {
+      'shoulders': '"Big Shoulders Display"',
+      'lexand': '"Lexend Deca"'
     }
   },
   plugins: [],
