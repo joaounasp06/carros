3d2d4afcb2efd34f1fa83ff65280db51eb25750d
João Nelson <joaonelson08@gmail.com>
2024-03-25 20:09:49 -0300

    projeto finalizado

--- src/App.jsx
+++ src/App.jsx
@@ -25,7 +25,7 @@ export default function app() {
           <img src={iconLuxury} alt="ícone dos carros de luxo" className="mb-7" />
           <h2 className="font-shoulders mb-7 uppercase text-4xl font-bold text-offwhitebg">Luxuosos</h2>
           <p className="font-lexend mb-7 text-offwhite lg:mb-20">Passeie nos melhores carros sem os preços exorbitantes. Desfrute do conforto aprimorado de um aluguel de luxo e chegue com estilo.</p>
-          <a className="font-lexend bg-offwhitebg block px-10 py-3 w-fit rounded-3xl text-dark-green border-2 border-offwhite-bg houver:bg-dark-green" href="https://www.webmotors.com.br/carros-de-luxo" target="_blank">Ver mais</a>
+          <a className="font-lexend  b bg-offwhitebg block px-10 py-3 w-fit rounded-3xl text-dark-green border-2 border-offwhite-bg houver:bg-dark-green" href="https://www.webmotors.com.br/carros-de-luxo" target="_blank">Ver mais</a>
         </div>
       </section>
     </main>
